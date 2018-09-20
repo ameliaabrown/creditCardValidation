6595fb4b277f837d87de154b85fadb085e20a955
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2018-09-20 14:37:12 -0600

    FINISHED YAYYY

--- Credit Card Validation/src/CreditCardValidation.java
+++ Credit Card Validation/src/CreditCardValidation.java
@@ -9,9 +9,6 @@ public class CreditCardValidation {
 	
 public static void main(String[] args) throws IOException
 {
-	pathOne();
-	pathTwo();
-
 		//pick a path
 			Scanner userPath = new Scanner(System.in);
 			System.out.println("1. Would you like to enter a credit card number to check?" 
@@ -19,6 +16,7 @@ public static void main(String[] args) throws IOException
 			int userChoice = userPath.nextInt();
 			long [] userNumberArray = new long [16];
 	
+			
 	if (userChoice == 1)
 	{
 		pathOne();
@@ -27,13 +25,12 @@ public static void main(String[] args) throws IOException
 	{
 		pathTwo();
 	}
+
 	
 }
 public static void pathOne()
 {
-	if (userChoice == 1)
-		{
-			
+
 			System.out.println("Please enter your credit card number below.");
 			Scanner userInput = new Scanner(System.in);
 			long userNumber = userInput.nextLong();
@@ -94,24 +91,67 @@ public static void pathOne()
 			}
 		
 		}
-}
+
 
 public static void pathTwo() throws IOException
 	{
-		
-		if (userChoice == 2)
-		{
+			int counter = 0;
 			Scanner importedCardNumbers = new Scanner (new File ("creditCardNumbers.txt"));
-			while(importedCardNumbers.hasNext())
-			{
-			System.out.println(importedCardNumbers.next());
-			}
 			
-		}
+			while (importedCardNumbers.hasNext())
+			{
+				long userNumber = importedCardNumbers.nextLong();
+				
+				for (int x = 15; x>= 0; x--)
+				{
+					long originalDigitStrip = userNumber %10;
+					userNumberArray [x] = originalDigitStrip;
+					userNumber = userNumber/10;
+					
+				}
+				
+				for (int j = 0; j < 16; j = j+2)
+				{
+					userNumberArray[j] = userNumberArray[j] *2; 
+					//System.out.println(userNumberArray[i]);
+					
+				}
+				
 		
-	
+				// add up doubled numbers 
+				for (int k = 0; k < 16; k++)
+				{
+					if (k%2 == 0)
+					{
+						if (userNumberArray[k] > 9)
+						{
+							long secondDigit = userNumberArray[k] % 10;
+							long firstDigit = (int) userNumberArray[k] / 10;
+							long addDigits = secondDigit + firstDigit;
+							userNumberArray[k] = addDigits;
+						
+						}
+					}
+				}
+					//System.out.println(Arrays.toString(userNumberArray));
+				
+				//find the sum 
+				long sum = 0;
+				for (int s = 0; s < 16; s++)
+				{
+					sum = sum + userNumberArray[s];
+				}
+				//System.out.println("The sum is " + sum);
+		
+				
+				//tell them if number is valid 
+				if (sum%10 == 0)
+				{
+					counter++;
+				}
+				
+			}
+			System.out.println("There are " + counter + " valid credit card numbers in the file. ");
 	}
 }
-
-				
 
